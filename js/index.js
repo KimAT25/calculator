c946455b48fc7dd4332d01b0b18e194d48bd213e
Igor Karpyn <kimat@ukr.net>
2019-11-14 21:43:28 +0000

    updated index.js

--- js/index.js
+++ js/index.js
@@ -22,6 +22,24 @@ function clearLastDigit() {
     const numbers = display.value;
     display.value = numbers.slice(0,-1);
 }
-
-
+document.querySelector('.calc .minus-number')
+    .addEventListener('click', minNumbr);
+function minNumbr() {
+    const minNum = parseFloat(display.value);
+    display.value = minNum * (-1);
+}
+document.querySelector('.calc .procent')
+    .addEventListener('click', procentNumbr);
+function procentNumbr() {
+    const minNum = parseFloat(display.value);
+    display.value = minNum / 100;
+}
+// document.querySelector('.calc .operation')
+//     .addEventListener('click', oneClick);
+// function oneClick() {
+//     const onlyOneClick = document.querySelector('.calc .operation')
+//     if(display.value === onlyOneClick) {
+//         return    
+//     };
+// }
 
